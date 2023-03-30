e285483d6d212c235884365afd35fc47c45481b6
donvladon2310 <vibliy.vlad@gmail.com>
2023-03-31 01:14:25 +0300

    hw02 fin

---
+++ helpers/HttpError.js
@@ -0,0 +1,13 @@
+const errorMesssages = {
+    400: "missing fields",
+    404: "Not found"
+}
+
+
+const HttpError = (status, message = errorMesssages[status]) => {
+    const error = new Error(message);
+    error.status = status;
+    return error
+}
+
+module.exports = HttpError;
